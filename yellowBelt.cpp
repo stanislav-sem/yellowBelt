e260db19075e5da3f38bf5b1837756af89970736
stanislav-sem <crazykvark@gmail.com>
2019-02-21 19:48:11 +0300

    almost1

--- yellowBelt.cpp
+++ yellowBelt.cpp
@@ -5,6 +5,28 @@
 #include <set>
 using namespace std;
 
+/*
+[{"IN_PROGRESS": 5}, {}]
+[{"TESTING": 5}, {}]
+[{"DONE": 1}, {"TESTING": 4}]
+[]
+[{"IN_PROGRESS": 2}, {"NEW": 3, "TESTING": 4, "DONE": 1}]
+{"NEW": 3, "IN_PROGRESS": 2, "TESTING": 4, "DONE": 1}
+[{"IN_PROGRESS": 3, "TESTING": 1}, {"IN_PROGRESS": 1, "TESTING": 4, "DONE": 1}]
+{"NEW": 0, "IN_PROGRESS": 4, "TESTING": 5, "DONE": 1}
+
+Correct output:
+[]
+[{"IN_PROGRESS": 5}, {}]
+[{"TESTING": 5}, {}]
+[{"DONE": 1}, {"TESTING": 4}]
+[]
+[{"IN_PROGRESS": 2}, {"NEW": 3, "TESTING": 4}]
+{"NEW": 3, "IN_PROGRESS": 2, "TESTING": 4, "DONE": 1}
+[{"IN_PROGRESS": 3, "TESTING": 1}, {"IN_PROGRESS": 1, "TESTING": 4}]
+{"IN_PROGRESS": 4, "TESTING": 5, "DONE": 1}
+ */
+
 // ѕеречислимый тип дл€ статуса задачи
 enum class TaskStatus {
 	NEW,
@@ -16,9 +38,15 @@ enum class TaskStatus {
 // “ип-синоним дл€ map<TaskStatus, int> позвол€ющий хранить количество задач данного статуса
 using TasksInfo = map<TaskStatus, int>;
 
-//void UpdateTask(int& tasks, int& task_count) {
-//	if
-//}
+TasksInfo ZeroElErase(TasksInfo input) {
+	TasksInfo result;
+	for (auto el : {TaskStatus::NEW, TaskStatus::IN_PROGRESS, TaskStatus::TESTING, TaskStatus::DONE }) {
+		if (input[el] != 0) {
+			result[el] = input[el];
+		}
+	}
+	return result;
+}
 
 class TeamTasks {
 public:
@@ -35,21 +63,27 @@ public:
 	// ќбновить статусы по данному количеству задач конкретного разработчика
 	tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count) {
 		TasksInfo copy_tasks = vault[person];
-		TasksInfo updated_tasks, untouched_tasks;
+		TasksInfo updated_tasks;
 		for (int i = 0; i < task_count; i++) {
 			if (copy_tasks[TaskStatus::NEW] > 0) {
 				--copy_tasks[TaskStatus::NEW];
-				++updated_tasks[TaskStatus::NEW];
+				++updated_tasks[TaskStatus::IN_PROGRESS];
 				continue;
 			} else if (copy_tasks[TaskStatus::IN_PROGRESS] > 0) {
 				--copy_tasks[TaskStatus::IN_PROGRESS];
-				++updated_tasks[TaskStatus::IN_PROGRESS];
+				++updated_tasks[TaskStatus::TESTING];
 				continue;
 			} else if (copy_tasks[TaskStatus::TESTING] > 0) {
 				--copy_tasks[TaskStatus::TESTING];
-				++updated_tasks[TaskStatus::TESTING];
+				++updated_tasks[TaskStatus::DONE];
 			}
 		}
+		for (auto el : {TaskStatus::NEW, TaskStatus::IN_PROGRESS, TaskStatus::TESTING, TaskStatus::DONE }) {
+			vault[person][el] = updated_tasks[el] + copy_tasks[el];
+		}
+		updated_tasks = ZeroElErase(updated_tasks);
+		copy_tasks = ZeroElErase(copy_tasks);
+		return tie(updated_tasks, copy_tasks);
 	}
 
 
@@ -67,31 +101,23 @@ void PrintTasksInfo(TasksInfo tasks_info) {
 
 int main() {
 	TeamTasks tasks;
-	tasks.AddNewTask("Ilia");
-	for (int i = 0; i < 3; ++i) {
-		tasks.AddNewTask("Ivan");
+	for (int i = 0; i < 5; ++i) {
+		tasks.AddNewTask("Alice");
 	}
-	cout << "Ilia's tasks: ";
-	PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-	cout << "Ivan's tasks: ";
-	PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
-
 	TasksInfo updated_tasks, untouched_tasks;
+	tasks.PerformPersonTasks("Alice", 5);
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Alice"));
+	tasks.PerformPersonTasks("Alice", 5);
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Alice"));
+	tasks.PerformPersonTasks("Alice", 1);
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Alice"));
+	for (int i = 0; i < 5; ++i) {
+		tasks.AddNewTask("Alice");
+	}
+	tasks.PerformPersonTasks("Alice", 2);
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Alice"));
+	tasks.PerformPersonTasks("Alice", 4);
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Alice"));
 
-	cout << "-----------------" << endl;
-	tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
-	cout << "Updated Ivan's tasks: ";
-	PrintTasksInfo(updated_tasks);
-	cout << "Untouched Ivan's tasks: ";
-	PrintTasksInfo(untouched_tasks);
-	cout << "Ivan's tasks: ";
-	PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 
-
-//	cout << "-----------------" << endl;
-//	tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
-//	cout << "Updated Ivan's tasks: ";
-//	PrintTasksInfo(updated_tasks);
-//	cout << "Untouched Ivan's tasks: ";
-//	PrintTasksInfo(untouched_tasks);
 }

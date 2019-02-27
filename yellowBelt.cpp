c9440dc10683c156479ada453dd480f728a23112
stanislav-sem <crazykvark@gmail.com>
2019-02-27 21:16:20 +0300

    sdfa

--- yellowBelt.cpp
+++ yellowBelt.cpp
@@ -1,264 +1,152 @@
-/*
 #include <iostream>
-#include <string>
 #include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
-int main() {
-  int q;
-  cin >> q;
 
-  map<string, vector<string>> buses_to_stops, stops_to_buses;
-
-  for (int i = 0; i < q; ++i) {
-    string operation_code;
-    cin >> operation_code;
-
-    if (operation_code == "NEW_BUS") {
-
-      string bus;
-      cin >> bus;
-      int stop_count;
-      cin >> stop_count;
-      vector<string>& stops = buses_to_stops[bus];
-      stops.resize(stop_count);
-      for (string& stop : stops) {
-        cin >> stop;
-        stops_to_buses[stop].push_back(bus);
-      }
-
-    } else if (operation_code == "BUSES_FOR_STOP") {
-
-      string stop;
-      cin >> stop;
-      if (stops_to_buses.count(stop) == 0) {
-        cout << "No stop" << endl;
-      } else {
-        for (const string& bus : stops_to_buses[stop]) {
-          cout << bus << " ";
-        }
-        cout << endl;
-      }
-
-    } else if (operation_code == "STOPS_FOR_BUS") {
-
-      string bus;
-      cin >> bus;
-      if (buses_to_stops.count(bus) == 0) {
-        cout << "No bus" << endl;
-      } else {
-        for (const string& stop : buses_to_stops[bus]) {
-          cout << "Stop " << stop << ": ";
-          if (stops_to_buses[stop].size() == 1) {
-            cout << "no interchange";
-          } else {
-            for (const string& other_bus : stops_to_buses[stop]) {
-              if (bus != other_bus) {
-                cout << other_bus << " ";
-              }
-            }
-          }
-          cout << endl;
-        }
-      }
-
-    } else if (operation_code == "ALL_BUSES") {
-
-      if (buses_to_stops.empty()) {
-        cout << "No buses" << endl;
-      } else {
-        for (const auto& bus_item : buses_to_stops) {
-          cout << "Bus " << bus_item.first << ": ";
-          for (const string& stop : bus_item.second) {
-            cout << stop << " ";
-          }
-          cout << endl;
-        }
-      }
-
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
     }
+    first = false;
+    os << x;
   }
-
-  return 0;
+  return os << "}";
 }
-*/
 
-#include <string>
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <map>
-
-using namespace std;
-
-enum class QueryType {
-  NewBus,
-  BusesForStop,
-  StopsForBus,
-  AllBuses
-};
-
-struct Query {
-  QueryType type;
-  string bus;
-  string stop;
-  vector<string> stops;
-};
-
-istream& operator >> (istream& is, Query& q) {
-  string type;
-  is >> type;
-  if (type == "NEW_BUS") {
-	  q.type = QueryType::NewBus;
-	  int stops_count = 0;
-	  is >> q.bus >> stops_count;
-	  for (int i = 0; i < stops_count; i++) {
-		  string stop;
-		  is >> stop;
-		  q.stops.push_back(stop);
-	  }
-  } else if (type == "BUSES_FOR_STOP") {
-	  q.type = QueryType::BusesForStop;
-	  is >> q.stop;
-  } else if (type == "STOPS_FOR_BUS") {
-	  q.type = QueryType::StopsForBus;
-	  is >> q.bus;
-  } else if (type == "ALL_BUSES") {
-	  q.type = QueryType::AllBuses;
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
   }
-  return is;
+  return os << "}";
 }
 
-struct BusesForStopResponse {
-  vector<string> vec;
-};
-
-ostream& operator << (ostream& os, const BusesForStopResponse& r) {
-	if ( r.vec.size() == 0) {
-		os << "No stop";
-	} else {
-		bool first = true;
-		for (const auto&  el : r.vec) {
-			if (!first) {
-				os << ' ';
-			}
-			first = false;
-			os << el;
-		}
-	}
-	return os;
-}
-
-struct StopsForBusResponse {
-  vector<string> vec_for_stops;
-  map<string, vector<string>> map_for_buses;
-  string bus;
-};
-
-ostream& operator <<(ostream& os, const StopsForBusResponse& r) {
-	if (r.vec_for_stops.size() == 0) {
-		os << "No bus";
-	} else {
-		for (const auto& st : r.vec_for_stops) {
-			os << "Stop " << st << ": ";
-			if (r.map_for_buses.count(st) == 1) {
-				os << "no interchange";
-			} else {
-				for (const auto& b : r.map_for_buses.at(st)) {
-					os << b << " ";
-				}
-			}
-			os << endl;
-		}
-	}
-	return os;
-}
-
-struct AllBusesResponse {
-  map<string, vector<string>> all_buses;
-};
-
-ostream& operator << (ostream& os, const AllBusesResponse& r) {
-  if (r.all_buses.size() == 0 ) {
-	  os << "No buses";
-  } else {
-	  for (auto bus : r.all_buses) {
-		  os << "Bus " << bus.first << ": ";
-		  for (auto stop : bus.second) {
-			  os << stop << " ";
-		  }
-		  os << endl;
-	  }
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
   }
-  return os;
+  return os << "}";
 }
 
-class BusManager {
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+       os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
 public:
-	void AddBus(const string& bus, const vector<string>& stops) {
-		buses_to_stops[bus] = stops;
-		for (string stop : stops) {
-			stops_to_buses[stop].push_back(bus);
-		}
-	}
-
-	BusesForStopResponse GetBusesForStop(const string& stop) const {
-		BusesForStopResponse response;
-		if (stops_to_buses.count(stop) != 0) {
-//			cout << stops_to_buses.at(stop).size() << endl;
-			response.vec = stops_to_buses.at(stop);
-		}
-		return response;
-	}
-
-	StopsForBusResponse GetStopsForBus(const string& bus) const {
-		StopsForBusResponse response;
-		response.bus = bus;
-		if (buses_to_stops.count(bus) != 0) {
-			response.vec_for_stops = buses_to_stops.at(bus);
-			response.map_for_buses = stops_to_buses;
-		}
-		return response;
-  }
-
-  AllBusesResponse GetAllBuses() const {
-    AllBusesResponse response;
-    response.all_buses = buses_to_stops;
-    return response;
-  }
-private:
-  map<string, vector<string>> buses_to_stops, stops_to_buses;
-};
-
-// Не меняя тела функции main, реализуйте функции и классы выше
-
-int main() {
-  int query_count;
-  Query q;
-
-  cin >> query_count;
-
-  BusManager bm;
-  for (int i = 0; i < query_count; ++i) {
-    cin >> q;
-    switch (q.type) {
-    case QueryType::NewBus:
-    	cout << "Adding " << q.stops.size()<< endl;
-      bm.AddBus(q.bus, q.stops);
-      break;
-    case QueryType::BusesForStop:
-      cout << bm.GetBusesForStop(q.stop) << endl;
-      break;
-    case QueryType::StopsForBus:
-      cout << bm.GetStopsForBus(q.bus) << endl;
-      break;
-    case QueryType::AllBuses:
-      cout << bm.GetAllBuses() << endl;
-      break;
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
     }
   }
 
-  return 0;
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+
+
+int GetDistinctRealRootCount(double a, double b, double c) {
+	set<double> korni;
+	double DD = b * b - 4 * a * c;
+	if (DD < 0) {
+		return 0;
+	}
+	if (a == 0 && b != 0) {
+		korni = {0, -c/b};
+	} else if (a == 0 && b == 0) {
+		return 0;
+	} else {
+		double D = sqrt(DD);
+		double x1 = (-b + D)/(2 * a);
+		double x2 = (-b - D)/(2 * a);
+		set<double> korni = {x1, x2};
+	}
+//  cout << korni << endl;
+	return korni.size();
+}
+void Test1() {
+	//проверяем правильность решения в классическом случае
+	AssertEqual(GetDistinctRealRootCount(1, 3, -4), 2, "Test1.1");
+	AssertEqual(GetDistinctRealRootCount(-6, -5, -1), 2, "Test1.2");
 }
 
+void Test2() {
+	//c=0
+	AssertEqual(GetDistinctRealRootCount(1, -4, 0), 2, "Test2.1");
+	AssertEqual(GetDistinctRealRootCount(1, -9, 0), 2, "Test2.2");
+	AssertEqual(GetDistinctRealRootCount(1, 0, 0), 1, "Test2.3");
+}
+void Test3() {
+	//b=0
+	AssertEqual(GetDistinctRealRootCount(1, 0, -9), 2, "Test3.1");
+	AssertEqual(GetDistinctRealRootCount(1, 0, 9), 0, "Test3.2");
+	AssertEqual(GetDistinctRealRootCount(1, 0, 0), 1, "Test3.3");
+}
+void Test4() {
+	// a=0
+	AssertEqual(GetDistinctRealRootCount(0, 1, 1), 1, "Test4.1");
+	AssertEqual(GetDistinctRealRootCount(0, 1, 0), 1, "Test4.2");
+}
+
+
+int main() {
+  TestRunner runner;
+//  cout << GetDistinctRealRootCount(1, 3, -4);
+  runner.RunTest(Test1, "Test1");
+  runner.RunTest(Test2, "Test2");
+  runner.RunTest(Test3, "Test3");
+  runner.RunTest(Test4, "Test4");
+//  double a = 1, b = 0, c = 9;
+//  cout << sqrt(b*b - 4 * a * c);
+  return 0;
+}
